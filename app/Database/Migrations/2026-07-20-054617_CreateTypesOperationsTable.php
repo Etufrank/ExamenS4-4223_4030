a76a911951b221f0e1c35474ed8ca99cdf8e944b
tahiry <tahiryaritratiana@gmail.com>
2026-07-20 08:52:24 +0300

    typesOperation

--- app/Database/Migrations/2026-07-20-054617_CreateTypesOperationsTable.php
+++ app/Database/Migrations/2026-07-20-054617_CreateTypesOperationsTable.php
@@ -8,11 +8,38 @@ class CreateTypesOperationsTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'nom' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+                'unique' => true,
+            ],
+            'code' => [
+                'type' => 'VARCHAR',
+                'constraint' => 10,
+                'unique' => true,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('types_operations');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('types_operations', true);
     }
 }
