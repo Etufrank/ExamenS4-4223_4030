7b4130192c2215b61c83cdc78cc10c04116afb02
Etufrank <frankrazni@gmail.com>
2026-07-20 09:25:52 +0300

    model 4

---
+++ app/Models/PrefixeOperateur.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PrefixeOperateurModel extends Model
+{
+    protected $table            = 'prefixes_operateur';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $allowedFields    = ['prefixe', 'description'];
+    protected $useTimestamps    = true;
+    protected $createdField     = 'created_at';
+    protected $updatedField     = null;
+}
