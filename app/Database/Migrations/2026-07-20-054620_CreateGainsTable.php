71bba962eb189100b6f344997a94eaa537e03135
tahiry <tahiryaritratiana@gmail.com>
2026-07-20 08:47:17 +0300

    4 derniers migatrations

---
+++ app/Database/Migrations/2026-07-20-054620_CreateGainsTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateGainsTable extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
